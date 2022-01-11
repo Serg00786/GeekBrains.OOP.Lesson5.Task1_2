f0f41d74bab1b67637c74d29cd92aa13d146dc65
Serg <a_s_v86@mail.ru>
2022-01-11 21:25:41 +0300

    Initial

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekBrains.OOP.Lesson5.Task1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
