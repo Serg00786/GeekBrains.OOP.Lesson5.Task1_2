a4b8f24deeb7888c138f978c382bb4738b6db686
Serg <a_s_v86@mail.ru>
2022-01-12 22:25:14 +0300

    Add complexNumber class

---
+++ ComplexNumber.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekBrains.OOP.Lesson5.Task1_2
+{
+    class ComplexNumber
+    {
+        internal double r, i;
+        public static ComplexNumber operator + (ComplexNumber a, ComplexNumber b)
+        {
+            ComplexNumber complex = new ComplexNumber();
+            complex.r = a.r + b.r;
+            complex.i = a.i + b.i;
+            return complex;
+        }
+
+        public static ComplexNumber operator - (ComplexNumber a, ComplexNumber b)
+        {
+            ComplexNumber complex = new ComplexNumber();
+            complex.r = a.r - b.r;
+            complex.i = a.i - b.i;
+            return complex;
+        }
+
+        public static ComplexNumber operator *(ComplexNumber a, ComplexNumber b)
+        {
+            ComplexNumber complex = new ComplexNumber();
+            complex.r = a.r * b.r;
+            complex.i = a.i * b.i;
+            return complex;
+        }
+
+        public static bool operator ==(ComplexNumber a, ComplexNumber b)
+        {
+            
+            bool Result = ReferenceEquals(a, b);
+            return Result;
+        }
+
+        public static bool operator !=(ComplexNumber a, ComplexNumber b)
+        {
+            return false;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,18 @@ namespace GeekBrains.OOP.Lesson5.Task1_2
     {
         static void Main(string[] args)
         {
+            ComplexNumber c1 = new ComplexNumber();
+            ComplexNumber c2 = new ComplexNumber();
+            c1.r = 4;
+            c1.i = 2;
+            c2.r = 5;
+            c2.i = 3;
+
+            var sum = c1 + c2;
+            var sub = c1 - c2;
+            var mul = c1 * c2;
+ 
+
         }
     }
 }
